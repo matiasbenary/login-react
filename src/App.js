557a4c395b8a3cc59a675f07a2fcb579c332844b
Matias Benary <matiasbenary@gmai.com>
2020-02-27 10:24:59 -0300

    :pencil: Se creo componente Login y logica basica

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react'
+import Login from './components/Login'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [auth,setAuth] = useState(false);
+    console.log(auth?"Logenado":"sin Logear");
+    return (
+        <div>
+            {auth
+            ?"Logeado"
+            :<Login setAuth={setAuth}/>}
+        </div>
+    )
 }
 
-export default App;
+export default App
---
+++ src/components/Login/index.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import "./login.scss";
+
+const Login = ({setAuth}) => {
+  const [dataLogin, setDataLogin] = useState({});
+
+  const saveDataLogin = e => {
+    const { name, value } = e.target;
+    setDataLogin({
+      ...dataLogin,
+      [name]: value
+    });
+  };
+
+  const onSubmit = e => {
+    e.preventDefault();
+    const isAuth = (dataLogin.username==="matias"
+        && dataLogin.password ==="123");
+    
+        setAuth(isAuth);
+
+  };
+  return (
+    <div className="body">
+      <form className="login" onSubmit={onSubmit}>
+        <h2 className="login__title">Login</h2>
+
+        <div className="login__inputs">
+          <label htmlFor="username" className="login__label">
+            Usuario
+            <input
+              type="text"
+              name="username"
+              className="login__input"
+              onChange={saveDataLogin}
+            />
+          </label>
+        </div>
+        <div className="login__inputs">
+          <label htmlFor="password" className="login__label">
+            Password
+            <input
+              type="password"
+              name="password"
+              className="login__input"
+              onChange={saveDataLogin}
+            />
+          </label>
+        </div>
+        <div className="login__buttons">
+          <button className="login__buttons__login" type="submit">
+            iniciar sesion
+          </button>
+          <button className="login__buttons__lost"> olvide mi clave</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+export default Login;
